28ad358897e287273cb22e635cb498ac1ec9bcef
Ваше Имя <kubatbekadilet20@gmail.com>
2022-04-15 21:23:20 +0600

    first commit

---
+++ jorgo.sql
@@ -0,0 +1,19 @@
+GREATE DATABASE jorgo;
+GREATE TABLE jorgo_taxi(
+    id INTEGER, 
+    marka TEXT, 
+    model VARGER, 
+    toplivo TEXT, 
+    rashod INTEGER);
+INSORT INTO jorgo_taxi(
+    id, marka, model, toplivo, rashod) 
+    VELUES (
+        1, 
+        MERSEDES, 
+        c500, 
+        benzin, 9);
+
+
+
+
+
